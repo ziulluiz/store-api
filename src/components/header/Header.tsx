39bfb8b4c7f2696a7a86032f52b109dc19d70eec
Guzman <luis.guzman2@firstdata.com>
2024-01-23 16:06:36 -0600

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import fiservLogo from "../../assets/fiserv_logo_orange_rgb.jpg";
+
+export const Header= () =>{
+    return(
+        <header>
+      <div className=" header_container">
+        <div className="logo_container">
+          <img src= {fiservLogo} alt="me" className='fiserv_logo'/>
+        </div>
+      </div>
+    </header>
+    )
+
+}
